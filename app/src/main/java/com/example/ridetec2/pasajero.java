2c7348819f2e14cb6c1c83ba4906b9cb7073b7f2
Manulz <ema.estrada98@gmail.com>
2019-11-25 21:27:29 -0600

    RideTec

--- app/src/main/java/com/example/ridetec2/pasajero.java
+++ app/src/main/java/com/example/ridetec2/pasajero.java
@@ -2,12 +2,16 @@ package com.example.ridetec2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class pasajero extends AppCompatActivity {
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pasajero);
+        lv=findViewById(R.id.lv_viajes);
+
     }
 }
